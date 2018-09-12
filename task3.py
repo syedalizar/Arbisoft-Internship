d57bd37e4f06a2510c52a8b573bbacede6111fd6
syedalizar <43003571+syedalizar@users.noreply.github.com>
2018-09-12 12:45:10 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,5 +1,5 @@
 d = {2:3, 1:89, 4:5, 3:0}
 sd = dict(sorted(d.items())) #simplest method to sort simple dictionaries
-#more complex ,ethod used in task 1
+#more complex method used in task 1
 
 print sd
